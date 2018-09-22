b2d490419bdfc1e84e722e2e942f2999966b2897
Andres Barro <andresbarroe+git@gmail.com>
2018-09-21 23:32:11 -0500

    Home Work

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1 class="cover-heading">My To Do App</h1>
+    <form @submit.prevent="addTask">
+      <input v-model="newTaskName" type="text" class="form-control">
+      <input type="submit" value="Agregar Tarea" class="btn btn-lg btn-block btn-primary">
+    </form>
+
+    <tasks-list
+      :title="'Pendientes'"
+      :task-list="tasksPending"
+      @completar="toggleTasks"/>
+
+    <tasks-list
+      :title="'Completados'"
+      :task-list="tasksComplete"
+      @completar="toggleTasks"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TasksList from './components/TasksList'
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    TasksList
+  },
+  data () {
+    return {
+      newTaskName: '',
+      tasks: [
+        {
+          name: 'Tarea 1',
+          done: false
+        },
+        {
+          name: 'Tarea 2',
+          done: false
+        },
+        {
+          name: 'Tarea 2',
+          done: false
+        }
+      ]
+    }
+  },
+  created(){
+    let tmp = localStorage.getItem('tasks')
+    if (tmp != null) {
+      this.tasks = JSON.parse(tmp)
+    } else {
+      this.tasks = []
+    }
+  },
+  methods: {
+    toggleTasks (task) {
+      task.done = !task.done
+      localStorage.setItem('tasks', JSON.stringify(this.tasks))
+    },
+    addTask () {
+      if (!this.newTaskName) return
+      this.tasks.push({ name: this.newTaskName, done: false })
+      this.newTaskName = ''
+      localStorage.setItem('tasks', JSON.stringify(this.tasks))
+    }
+  },
+  computed: {
+    tasksPending () {
+      return this.tasks.filter(task => !task.done)
+    },
+    tasksComplete () {
+      return this.tasks.filter(task => task.done)
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TasksList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h3>{{ title }}</h3>
+      <ul>
+        <li v-for="(task, index) in taskList"
+            :key="index"
+            class="jumbotron">
+          <h3>{{ task.name }} - {{ task.done ? 'Completado' : 'Pendiente' }}</h3>
+          <button v-if="!task.done" type="button" @click="toggleTasks(task)" class="btn btn-block btn-primary">Completar</button>
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    taskList: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    toggleTasks (task) {
+      this.$emit('completar', task)
+    }
+  }
+}
+</script>
